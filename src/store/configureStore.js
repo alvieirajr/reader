ed0e82f023ff474aed9a0a28ac946f08f4408279
alvieirajr <alvieirajr@gmail.com>
2017-11-24 17:28:57 -0300

    feat: Creation of configureStore.js

---
+++ src/store/configureStore.js
@@ -0,0 +1,12 @@
+// ./src/store/configureStore.js
+import {createStore, compose, applyMiddleware} from 'redux';
+// Import thunk middleware
+import thunk from 'redux-thunk';
+import rootReducer from '../reducers';
+
+export default function configureStore(initialState) {
+  return createStore(rootReducer, initialState,
+    // Apply to store
+    applyMiddleware(thunk)
+  );
+}

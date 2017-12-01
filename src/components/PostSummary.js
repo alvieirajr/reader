bea5274bb75ae2cac11b58efce3bba9712b2fdb6
Antonio <alvieirajr@gmail.com>
2017-11-30 22:10:22 -0300

    refact: Transform class component PostSummary to a function component

--- src/components/PostSummary.js
+++ src/components/PostSummary.js
@@ -5,37 +5,21 @@ import EditIcon from 'react-icons/lib/ti/pencil'
 
 import { ListGroupItem, Row, Col } from 'react-bootstrap'
 
-class PostSummary extends Component { 
-    
-    componentDidMount() {
-        this.setState({
-            ...this.props
-        })
-    }
-
-    render() {
-        if (this.state) {
-        return (
-            <ListGroupItem className='bg-gray without-v-padding '>
-                <Row className='footer'> 
-                    <Col xs={6} className='text-left '>
-                        <span className='text-black'> <CommentIcon size={16} /> {this.state.commentCount} </span>
-                    </Col>
-                    <Col xs={6} className='text-right'>
-                  {/*}  <span className='text-black'><EditIcon size={18} /></span>
-                    <span className='text-black'> <TrashIcon size={18} /> </span> 
-                    */}
-                    </Col>
-                </Row>                    
-            </ListGroupItem>            
-        )
-        } else {
-            return (
-                <div></div>
-            )
-        }
-
-    }
+function PostSummary (props) {    
+    return (
+        <ListGroupItem className='bg-gray without-v-padding '>
+            <Row className='footer'> 
+                <Col xs={6} className='text-left '>
+                    <span className='text-black'> <CommentIcon size={16} /> {props.commentCount} </span>
+                </Col>
+                <Col xs={6} className='text-right'>
+                {/*}  <span className='text-black'><EditIcon size={18} /></span>
+                <span className='text-black'> <TrashIcon size={18} /> </span> 
+                */}
+                </Col>
+            </Row>                    
+        </ListGroupItem>            
+    )
 }
 
 export default PostSummary;

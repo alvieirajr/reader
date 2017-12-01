43fb2b4d8d644d4758bcc42daf9d7963425f2778
Antonio <alvieirajr@gmail.com>
2017-11-30 21:54:21 -0300

    feat: Votting logic

--- src/components/PostCommandBar.js
+++ src/components/PostCommandBar.js
@@ -10,49 +10,42 @@ import IconThumbsOUp from 'react-icons/lib/fa/thumbs-o-up'
 
 class PostCommandBar extends Component { 
 
-    state = {
-        timestamp : 'Há 9h',
-        title : 'O que eu penso sobre isso?',
-        body : 'klajsdklas jdlka sjdklajsdjlkjlk alskdjaklsj aksldjl aksd aklsjkjas alskj',
-        author : 'Thiago Antunes',
-        category: 'science',    
-        voteScore : 4,
-        commentCount : 0,
-        vote : 0,
-        canIRemove : false
-      }
+    constructor(props) {
+        super(props);    
+    }
+
+    componentDidMount() {
+        this.setState( {
+            ...this.props,
+        })        
+    }
 
     toggleVoteUp = () => {
-        console.log(this.state.voteScore);
         this.setState ({
-                vote : this.state.vote > 0 ? 0 : 1,
-                voteScore : (this.state.voteScore) + (this.state.vote+1 == 0 ? 2 : this.state.vote+1 == 1 ? 1 : -1),
-            })
+            voteScore : this.state.voteScore + 1
+        })
     }
     
     toggleVoteDown = () => {
-        console.log(this.state.voteScore + this.state.vote);
         this.setState ({
-                vote : this.state.vote < 0 ? 0 : -1,
-                voteScore : this.state.voteScore + (this.state.vote-1 == 0 ? -2 : this.state.vote-1 == -1 ? -1 : 1),
-            })
-        }    
+            voteScore : this.state.voteScore - 1
+        })
+    }    
 
     render() {
-
-        return (
-            <div>
+        if (this.state) {
+            return (
+            <div>               
                 <Col xs={4} >
-                    <ButtonToolbar >                    
-                    
+                    <ButtonToolbar >                                        
                         <Button className='without-h-padding'  bsStyle="link" onClick={this.toggleVoteUp}>  
-                            { ((this.state.vote <= 0)) ?  <IconThumbsOUp size={18} /> : <IconThumbsUp size={18} />}
+                            <IconThumbsOUp size={18} />
                             </Button>
                             <Button className='without-h-padding'  bsStyle="link"> 
                             &nbsp;{this.state.voteScore}&nbsp;
                             </Button>
                             <Button className='without-h-padding' bsStyle="link" onClick={this.toggleVoteDown}> 
-                            { ((this.state.vote >= 0)) ? <IconThumbsODown size={18} /> : <IconThumbsDown size={18} />}                    
+                            <IconThumbsODown size={18} /> 
                             </Button>
                     </ButtonToolbar>
                 </Col>                
@@ -65,10 +58,13 @@ class PostCommandBar extends Component {
                     <ButtonToolbar>
                         <Button className='' bsStyle="link"><TrashIcon size={18} />Remove</Button>            
                     </ButtonToolbar>
-                </Col>            
-            </div>
-            
-        )
+                </Col>      
+            </div>)
+        } else { 
+            return (
+                <div></div>
+            )
+        }                
     }
 }
 
--- src/reducers/PostReducer.js
+++
@@ -1,6 +0,0 @@
-function reducer(state = {}, action) { 
-    switch(active.type) {
-        case "GET_ALL_POSTS":
-            return {};
-    }
-}

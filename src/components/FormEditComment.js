51638fce81f7d5c95a62141382bbde711cda3eb4
Antonio <alvieirajr@gmail.com>
2018-06-08 22:39:04 -0300

    feat: Component to edit comments.

--- src/components/FormEditComment.js
+++ src/components/FormEditComment.js
@@ -3,29 +3,49 @@ import FaQuestionCircle from 'react-icons/lib/fa/question-circle'
 import { Modal, Button } from 'react-bootstrap';
 
 class FormEditComment extends React.Component {
-    render() {
-      return (          
-        <Modal
-          {...this.props}
-          bsSize="large"
-          aria-labelledby="contained-modal-title-lg"
-          onHide={this.props.noOperation}
-        >
-          <Modal.Header closeButton>
-            <Modal.Title id="contained-modal-title-lg">{this.props.title}</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>            
-            <p>
-                <FaQuestionCircle size={32}/>&nbsp;&nbsp;&nbsp;&nbsp;{this.props.menssage}
-            </p>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button onClick={this.props.yesOperation}>Yes</Button>
-            <Button bsStyle="primary" onClick={this.props.noOperation}>No</Button>
-          </Modal.Footer>
-        </Modal>
-      );
+
+  _handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      if (this.state.value.trim().length > 0) {
+        this.props.XXXComment({
+          parentId: this.props.parentId,
+          body: this.state.value
+        })
+        this.setState({ value: '' });
+        console.log(this)
+
+      }
+      e.preventDefault();
     }
   }
 
-  export default FormEditComment;
+  handleChange = (e) => {
+    this.setState({ value: e.target.value });
+  }
+
+  render() {
+    return (
+      <Modal
+        {...this.props}
+        bsSize="large"
+        aria-labelledby="contained-modal-title-lg"
+        onHide={this.props.noOperation}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-lg">{this.props.title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <FormGroup className='without-bottom-margin'>
+            <FormControl componentClass="textarea" className='text-area' type="text" placeholder='Write a comment...' wrap="hard" value={this.state.value} onChange={this.handleChange} onKeyPress={this._handleKeyPress} />
+          </FormGroup>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={this.props.yesOperation}>Yes</Button>
+          <Button bsStyle="primary" onClick={this.props.noOperation}>No</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+}
+
+export default FormEditComment;
